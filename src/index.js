f06b7ab5a96902f12cfda08998372841a2023c82
Mikita Kryvitski <solairerove@gmail.com>
2019-06-15 23:48:10 +0300

    feature: create simple comment component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail'
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail
+                image={faker.image.avatar()}
+                author="Sam"
+                timeAgo="Today at 6:00PM"
+                comment="Nice blog post!"
+            />
+            <CommentDetail author="John" timeAgo="Today at 4:45AM" image={faker.image.avatar()} comment="Awesome blog post!" />
+            <CommentDetail author="Doe" timeAgo="Today at 3:10AM" image={faker.image.avatar()} comment="Я обосрался !" />
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
