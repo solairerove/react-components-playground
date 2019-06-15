a887dbf30a72c3593b5e82589f61bb2bb98d5467
Mikita Kryvitski <solairerove@gmail.com>
2019-06-16 00:45:11 +0300

    feature: create reusable component and use props children

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail'
 import faker from 'faker';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                image={faker.image.avatar()}
-                author="Sam"
-                timeAgo="Today at 6:00PM"
-                comment="Nice blog post!"
-            />
+            <ApprovalCard >
+                <CommentDetail
+                    image={faker.image.avatar()}
+                    author="Sam"
+                    timeAgo="Today at 6:00PM"
+                    comment="Nice blog post!"
+                />
+            </ApprovalCard>
+
             <CommentDetail author="John" timeAgo="Today at 4:45AM" image={faker.image.avatar()} comment="Awesome blog post!" />
             <CommentDetail author="Doe" timeAgo="Today at 3:10AM" image={faker.image.avatar()} comment="Я обосрался !" />
+
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure ?
+                </div>
+            </ApprovalCard>
         </div>
     );
 };
